894f786fab118dffacf6f86d31930be02113d7e7
ARADHYA NEGI <an8038@srmist.edu.in>
2026-01-15 11:16:59 +0530

    Add files via upload

---
+++ Secret_Auction(Beginner_09).py
@@ -0,0 +1,23 @@
+import logo
+
+print(logo.logo)
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    return winner
+bidders={}
+choice="yes"
+while choice == "yes":
+        name = input("Enter your name: ")
+        bid = int(input("Enter your bid amount: $"))
+        bidders[name] = bid
+        choice = input("Are there any other bidders? Type 'yes' or 'no'. ").lower()
+        print("\n" * 70)
+        if choice == "no":
+            highest_bidder = find_highest_bidder(bidders)
+            print(f"The winner is {highest_bidder} with a bid of ${bidders[highest_bidder]}")
